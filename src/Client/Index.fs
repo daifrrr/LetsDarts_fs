665708f1455bd1fbf79eafa63d8fa92403453484
david z <d.zschornak@gmail.com>
2022-04-15 16:09:06 +0200

    deleted:    package-lock.json 	modified:   package.json 	modified:   src/Client/Index.fs 	modified:   src/Client/public/style.css 	modified:   yarn.lock

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -5,7 +5,7 @@ open Fable.Remoting.Client
 open Shared
 
 
-let numbs = seq {20, 1, 18, 4, 13, 6, 10, 15, 2, 17, 3, 19, 7, 16, 8, 11, 9, 12, 5}
+let numbs = seq { 20; 1; 18; 4; 13; 6; 10; 15; 2; 17; 3; 19; 7; 16; 8; 11; 14; 9; 12; 5 }
 
 type Model = { Todos: Todo list; Input: string; }
 
@@ -95,48 +95,70 @@ let sections startAngle endAngle color =
     |> List.ofSeq
     |> Fable.React.Helpers.ofList
 
+let redSections (dartnumber: int, angle: float) (color: string * string) =
+    Browser.Dom.console.log(angle)
+    Svg.g [
+        svg.id (string $"f{dartnumber}")
+        svg.transform.rotate angle
+        svg.children [
+            Svg.use' [ svg.id (string $"s{dartnumber}"); svg.href "#single"; svg.height 500; svg.width 500; svg.y 0; svg.x 0; svg.fill (fst color); svg.onClick handleClick]
+            Svg.use' [ svg.id (string $"d{dartnumber}"); svg.href "#double"; svg.height 500; svg.width 500; svg.y 0; svg.x 0; svg.fill (snd color); svg.onClick handleClick]
+            Svg.use' [ svg.id (string $"t{dartnumber}"); svg.href "#triple"; svg.height 500; svg.width 500; svg.y 0; svg.x 0; svg.fill (snd color); svg.onClick handleClick]
+        ]
+    ]
+
 
 let containerBox (model: Model) (dispatch: Msg -> unit) =
     Bulma.box [
-        Bulma.container [
-            Svg.svg [
-                svg.id "svg2"
-                svg.viewBox (-250, -250, 500, 500)
-                unbox("version", "1.0")
-                unbox("xmlns:rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-                unbox("xmlns", "http://www.w3.org/2000/svg")
-                unbox("xmlns:cc", "http://creativecommons.org/ns#")
-                unbox("xmlnsXlink", "http://www.w3.org/1999/xlink") // Error when xmlns:xlink
-                unbox("xmlns:dc", "http://purl.org/dc/elements/1.1/")
-                unbox("xmlns:svg", "http://www.w3.org/2000/svg")
-                svg.children [
-                    Svg.defs [
-                        svg.id "defs6"
+        prop.width 100
+        prop.height 100
+        prop.children [
+            Bulma.container [
+                prop.className "dartboardContainer"
+                prop.children [
+                    Svg.svg [
+                        svg.id "svg2"
+                        svg.viewBox (-250, -250, 500, 500)
+                        unbox("version", "1.0")
+                        unbox("xmlns:rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+                        unbox("xmlns", "http://www.w3.org/2000/svg")
+                        unbox("xmlns:cc", "http://creativecommons.org/ns#")
+                        unbox("xmlnsXlink", "http://www.w3.org/1999/xlink") // Error when xmlns:xlink
+                        unbox("xmlns:dc", "http://purl.org/dc/elements/1.1/")
+                        unbox("xmlns:svg", "http://www.w3.org/2000/svg")
                         svg.children [
-                            Svg.line [ svg.id "refwire"; svg.y2 167.4; svg.y1 16.2; svg.stroke "#c0c0c0"; svg.x2 26.52; svg.x1 2.566 ]
-                            Svg.path [ svg.id "SLICE"; svg.strokeWidth 0; svg.d "M 0 0 L 39.108616 246.922085 A 250 250 0 0 0 -39.108616 246.922085 L 0 0 Z" ]
-                            Svg.path [ svg.id "DLICE"; svg.strokeWidth 0; svg.d "M 31.286893 197.537668 L 39.108616 246.922085 A 250 250 0 0 1 -39.108616 246.922085 L -31.286893 197.537668 A 200 200 0 0 0 31.286893 197.537668 Z" ]
-                            Svg.path [ svg.id "TLICE"; svg.strokeWidth 0; svg.d "M 15.643447 98.768834 L 23.465169 148.153251 A 150 150 0 0 1 -23.465169 148.153251 L -15.643447 98.768834 A 100 100 0 0 0 15.643447 98.768834 Z" ]
-                            Svg.use' [ svg.id "single"; unbox("xlinkHref", "#SLICE"); svg.height 500; svg.width 500; svg.y 0; svg.x 0 ]
-                            Svg.use' [ svg.id "double"; unbox("xlinkHref", "#DLICE"); svg.height 500; svg.width 500; svg.y 0; svg.x 0 ]
-                            Svg.use' [ svg.id "triple"; unbox("xlinkHref", "#TLICE"); svg.height 500; svg.width 500; svg.y 0; svg.x 0 ]
-                        ]
-                    ]
-                    Svg.g [
-                        svg.id "g14"
-                        svg.transform.matrix (1, 0, 0, -1, -1, 0)
-                        svg.children [
-                            Svg.g [
-                                svg.id "dartboard"
+                            Svg.defs [
+                                svg.id "defs6"
                                 svg.children [
-                                    // WHATABOUT:
-                                    // let! redSections =
-                                    // (seq of dartsnumber, seq of corresponding angles)
-                                    // ||> Seq.map2 (fun dartnumber, angle -> (dartnumber, angle))
-                                    // |> Seq.indexed
-                                    // |> Seq.filter (fun (i, (_,_) -> i % 2 = 0)))
-                                    sections 0. 324. ("#000000", "#ff0000")
-                                    sections 18. 342. ("#ffffff", "#00ff00")
+                                    Svg.line [ svg.id "refwire"; svg.y2 167.4; svg.y1 16.2; svg.stroke "#c0c0c0"; svg.x2 26.52; svg.x1 2.566 ]
+                                    Svg.path [ svg.id "SLICE"; svg.stroke "#bbbbbb" ;svg.strokeWidth 2; svg.d "M 0 0 L 39.108616 246.922085 A 250 250 0 0 0 -39.108616 246.922085 L 0 0 Z" ]
+                                    Svg.path [ svg.id "DLICE"; svg.stroke "#bbbbbb" ;svg.strokeWidth 2; svg.d "M 31.286893 197.537668 L 39.108616 246.922085 A 250 250 0 0 1 -39.108616 246.922085 L -31.286893 197.537668 A 200 200 0 0 0 31.286893 197.537668 Z" ]
+                                    Svg.path [ svg.id "TLICE"; svg.stroke "#bbbbbb" ;svg.strokeWidth 2; svg.d "M 15.643447 98.768834 L 23.465169 148.153251 A 150 150 0 0 1 -23.465169 148.153251 L -15.643447 98.768834 A 100 100 0 0 0 15.643447 98.768834 Z" ]
+                                    Svg.use' [ svg.id "single"; unbox("xlinkHref", "#SLICE"); svg.height 500; svg.width 500; svg.y 0; svg.x 0 ]
+                                    Svg.use' [ svg.id "double"; unbox("xlinkHref", "#DLICE"); svg.height 500; svg.width 500; svg.y 0; svg.x 0 ]
+                                    Svg.use' [ svg.id "triple"; unbox("xlinkHref", "#TLICE"); svg.height 500; svg.width 500; svg.y 0; svg.x 0 ]
+                                ]
+                            ]
+                            Svg.g [
+                                svg.id "g14"
+                                svg.transform.matrix (1, 0, 0, -1, -1, 0)
+                                svg.children [
+                                    Svg.g [
+                                        // TODO: Seq<a'> Seq<b'> must have same length, check!
+                                        svg.id "dartboard"
+                                        svg.children [
+                                                (numbs, seq { 0. .. 18. .. 342. })
+                                                ||> Seq.map2 (fun n a -> (n, a))
+                                                |> Seq.indexed
+                                                |> Seq.map(fun (i, (n, a)) -> match i % 2 = 0 with
+                                                                                | true -> redSections (n, a) ("#0", "#ff0000")
+                                                                                | _ -> redSections (n, a) ("#ffffff", "#00ff00"))
+                                                |> List.ofSeq
+                                                |> Fable.React.Helpers.ofList
+                                                Svg.circle [ svg.cx 0; svg.cx 0; svg.r 50; svg.stroke "#bbbbbb"; svg.strokeWidth 2; svg.fill "#00ff00"]
+                                                Svg.circle [ svg.cx 0; svg.cx 0; svg.r 25; svg.stroke "#bbbbbb"; svg.strokeWidth 2; svg.fill "#ff0000"]
+                                        ]
+                                    ]
                                 ]
                             ]
                         ]
@@ -164,8 +186,8 @@ let view (model: Model) (dispatch: Msg -> unit) =
             Bulma.heroBody [
                 Bulma.container [
                     Bulma.column [
-                        column.is6
-                        column.isOffset3
+                        column.is10
+                        column.isOffset1
                         prop.children [
                             Bulma.title [
                                 text.hasTextCentered
