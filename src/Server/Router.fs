b0bedd1798dd486f41cae515413d01c0a3fa34fd
David_Z <d.zschornak@gmail.com>
2022-04-26 10:31:46 +0200

    prepare add LetsDartsCore

--- src/Server/Router.fs
+++ src/Server/Router.fs
@@ -6,15 +6,29 @@ open Fable.Remoting.Server
 
 open Shared
 
+type DartsGame() =
+    let history = ResizeArray<_>()
 
-module Storage =
-    let aBoolString = (true, "Here i am")
+    member _.GetGames() = List.ofSeq history
+
+    member _.GetLast() = List.ofSeq history |> List.tail
+
+    member _.AddGame(game: Game) = history.Add game
+
+let DartsGame = DartsGame()
+
+// let getCurrentPlayer (game:Game): Player =
+//     game.Players |> List.tryFind (fun p -> p.Legs.Head.Records.Length % 3 <> 0)
+
+// let calcNewGame (throw:string) =
+//     let currentPlayer = getCurrentPlayer DartsGame.GetLast
+//     currentPlayer
 
 let gameApi =
-    {
-      initGame = fun game -> async {
-            printfn "%A" game
-            return (Running, game)
-          }
-      sendThrow = fun str -> async {return Game.Default }
-    }
+    { initGame =
+        fun game ->
+            async {
+                DartsGame.AddGame game
+                return (Running, game)
+            }
+      sendThrow = fun str -> async { return Game.Default } }
