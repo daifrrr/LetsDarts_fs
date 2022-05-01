9f269dad1daab5e18c0b0d70d895ab10fc18b378
David_Z <d.zschornak@gmail.com>
2022-05-02 01:24:12 +0200

    Reformating
    Signed-off-by: David_Z <d.zschornak@gmail.com>

--- src/Shared/Helpers.fs
+++ src/Shared/Helpers.fs
@@ -9,14 +9,3 @@ module Logger =
             f |> ignore
         }
 
-let f = [
-            [
-                { CurrentScore = 121; Records = [('s', 0); ('s', 0); ('s', 0); ('t', 20); ('t', 20); ('t', 20)] }
-                { CurrentScore = 0; Records = [('d', 17); ('t', 9); ('t', 20); ('t', 20); ('t', 20); ('t', 20)] }
-            ]
-            [
-                { CurrentScore = 0; Records = [('d', 17); ('t', 20); ('t', 19); ('d', 25); ('d', 25); ('d', 25)] }
-                { CurrentScore = 121; Records = [('t', 20); ('t', 20); ('t', 20)] }
-            ]
-        ]
-
