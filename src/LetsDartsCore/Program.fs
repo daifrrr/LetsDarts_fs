ebf8cc9a477ee22bfcff01b53f0925e3ae757607
d.zschornak <d.zschornak@gmail.com>
2022-05-05 18:10:44 +0200

    refactoring while writing tests
    Signed-off-by: d.zschornak <d.zschornak@gmail.com>

--- src/LetsDartsCore/Program.fs
+++ src/LetsDartsCore/Program.fs
@@ -1,112 +1,117 @@
 ﻿namespace LetsDartsCore
 
-open System
-open System.Text.RegularExpressions
 open Shared
 
 module Game =
+    let replaceLast_1_to_3_ShotsWithShotZERO l =
+        match l.Records with
+        | [] -> { l with Records = List.replicate 3 Shot.ZERO }
+        | _ ->
+            let numberOfToDeleteFromRecord =
+                match ((%) l.Records.Length 3) with
+                | 0 -> 3
+                | n -> n
+
+            let oldRecord =
+                l.Records |> List.skip numberOfToDeleteFromRecord
+
+            let resetScore =
+                l.Records
+                |> List.take numberOfToDeleteFromRecord
+                |> List.fold (fun s t -> s + t.Result) 0
+
+            { l with
+                CurrentScore = ((-) l.CurrentScore resetScore)
+                Records = List.replicate 3 Shot.ZERO @ oldRecord }
+
+    let validateDoubleIn (s: Shot) =
+        match s with
+        | s when s.Factor = Double -> DoubleInSuccess
+        | _ -> DoubleInFail
+
+    let validateDoubleOut (s: Shot) =
+        match s with
+        | s when s.Factor = Double -> GameOver
+        | _ -> Overthrown
+
     let prependThrow l t = { l with Records = [ t ] @ l.Records }
 
-    let endRecordResetLastThrows l =
-        let numberOfToDeleteFromRecord =
-            match ((%) l.Records.Length 3) with
-            | 0 -> 3
-            | n -> n
+    let applyGameLogic l (s: Shot) g =
+        let newLeg = prependThrow l s
 
-        let oldRecord = l.Records |> List.skip numberOfToDeleteFromRecord
+        match (g.Mode, newLeg |> Leg.calcCurrentScore) ||> (-) with
+        | 0 when g.DoubleOut ->
+            match validateDoubleOut s with
+            | GameOver -> { newLeg with CurrentScore = newLeg |> Leg.calcCurrentScore }
+            | _ -> replaceLast_1_to_3_ShotsWithShotZERO l
+        | 0 when g.DoubleOut |> not -> { newLeg with CurrentScore = newLeg |> Leg.calcCurrentScore }
+        | sc when 0 > sc && g.DoubleOut |> not -> replaceLast_1_to_3_ShotsWithShotZERO l
+        | sc when 2 > sc && g.DoubleOut -> replaceLast_1_to_3_ShotsWithShotZERO l
+        | sc when (g.Mode - s.Result) = sc && g.DoubleIn ->
+            match validateDoubleIn s with
+            | DoubleInSuccess -> { newLeg with CurrentScore = newLeg |> Leg.calcCurrentScore }
+            | _ -> prependThrow l Shot.ZERO
+        | _ -> { newLeg with CurrentScore = newLeg |> Leg.calcCurrentScore }
 
-        let resetScore =
-            l.Records
-            |> List.take numberOfToDeleteFromRecord
-            |> List.fold (fun s t -> s + t.Result) 0
-
-        { l with
-            CurrentScore = ((+) l.CurrentScore resetScore)
-            Records = List.replicate 3 (Shot(Single, 0)) @ oldRecord }
-
-    let validateDoubleIn l (r:Shot) =
-        match l, r with
-        | l, r when r.Factor = Double && l.Records |> List.isEmpty ->
-            (DoubleInSuccess, { l with CurrentScore = ((-) l.CurrentScore r.Result) })
-        | l, r when
-            r.Factor = Double
-            && l.Records |> List.forall (fun e -> e = (Shot(Single, 0)))
-            ->
-            (DoubleInSuccess, { l with CurrentScore = ((-) l.CurrentScore r.Result) })
-        | _, r -> (DoubleInFail, l)
-
-    let validateDoubleOut l (r:Shot) =
-        match l, r with
-        | l, r when r.Factor = Double && ((-) l.CurrentScore r.Result) = 0 -> (GameOver, { l with CurrentScore = 0 })
-        | l, r when ((-) l.CurrentScore r.Result) < 2 -> (DoubleOutFail, l)
-        | _ -> (GameOn, { l with CurrentScore = ((-) l.CurrentScore r.Result) })
-
-    let applyGameLogic l (t:Shot) (dblI, dblO) =
-        match (dblI, validateDoubleIn l t) with
-        | true, (DoubleInSuccess, l) -> prependThrow l t
-        | true, (DoubleInFail, l) -> prependThrow l (Shot(Single, 0))
-        | _ ->
-            match (dblO, validateDoubleOut l t) with
-            | true, (DoubleOutFail, l) -> endRecordResetLastThrows l
-            | true, (GameOver, l) -> prependThrow l t
-            | _, (GameOn, l) -> prependThrow l t
-            // TODO: no doubleIN; noDoubleOut
-            | _ -> failwith "ruleset validation failed"
+    let getThrowCounter players =
+        Player.getLegsPerPlayer players
+        |> List.map (fun l -> l.Head.Records.Length)
+        |> List.reduce (fun acc l -> acc + l)
 
     let getCurrentPlayerIndex (players: Player list) : int =
-        let throwCounter =
-            Player.getLegsPerPlayer players
-            |> List.map (fun l -> l.Head.Records.Length)
-            |> List.reduce (fun acc l -> acc + l)
-
-        ((%) ((/) throwCounter 3) players.Length)
+        ((%) ((/) (players |> getThrowCounter) 3) players.Length)
 
 
     let calcNewGame (throw: string) (game: Game) =
-        let currentPlayer = game.Players[(Game.getPlayers game |> getCurrentPlayerIndex)]
+        let currentPlayer =
+            game.Players[(Game.getPlayers game |> getCurrentPlayerIndex)]
 
-        let currentPlayerLeg = Player.getCurrentLeg currentPlayer
+        let currentPlayerLeg =
+            Player.getCurrentLeg currentPlayer
 
-        let currentThrow = (throw |> parseThrow).Value
+        let currentThrow =
+            match (throw |> parseThrow) with
+            | Some s -> s
+            | None -> failwithf $"Could not {throw} parse throw"
 
-        let state =
-            applyGameLogic currentPlayerLeg currentThrow (game.DoubleIn, game.DoubleOut)
+        let modifiedLeg =
+            applyGameLogic currentPlayerLeg currentThrow game
 
-        let currentPoints =
-            state.Records
-            |> List.fold (fun m t -> m - t.Result) game.Mode
+        (GameOn, game)
+        //        let currentPoints =
+//            modifiedLeg.Records
+//            |> List.fold (fun m s -> m - s.Result) game.Mode
 
-        let players = Game.getPlayers game
+//        let players = Game.getPlayers game
 
-        let newPlayers =
-            players
-            |> List.map (fun p ->
-                match p = currentPlayer with
-                | true -> { p with Legs = [ state ] @ p.Legs.Tail }
-                | _ -> p)
+//        let newPlayers =
+//            players
+//            |> List.map (fun p ->
+//                match p = currentPlayer with
+//                | true -> { p with Legs = [ modifiedLeg ] @ p.Legs.Tail }
+//                | _ -> p)
 
 
-        let nextStep =
-            match currentPoints with
-            | 0 ->
-                match Player.getLegsPerPlayer newPlayers
-                      |> List.concat with
-                | l when l.Length < ((-)((*) game.Legs 2) 1)  ->
-                    printfn $"{l}"
-                    LegOver
-                | _ -> GameOver
-            | _ -> GameOn
-
-        match nextStep with
-        | LegOver ->
-            (nextStep,
-             { game with
-                 Players =
-                     newPlayers
-                     |> List.map (fun p ->
-                         { p with
-                             Legs =
-                                 [ { Leg.Default with CurrentScore = game.Mode } ]
-                                 @ p.Legs }) })
-        | GameOn -> (nextStep, { game with Players = newPlayers })
-        | _ -> (nextStep, { game with Players = newPlayers })
+//        let nextStep =
+//            match currentPoints with
+//            | 0 ->
+//                match Player.getLegsPerPlayer newPlayers |> List.concat with
+//                | l when l.Length < ((-) ((*) game.Legs 2) 1) ->
+//                    printfn $"{l}"
+//                    LegOver
+//                | _ -> GameOver
+//            | _ -> GameOn
+//
+//        match nextStep with
+//        | LegOver ->
+//            (nextStep,
+//             { game with
+//                 Players =
+//                     newPlayers
+//                     |> List.map (fun p ->
+//                         { p with
+//                             Legs =
+//                                 [ { Leg.Default with CurrentScore = game.Mode } ]
+//                                 @ p.Legs }) })
+//        | GameOn -> (nextStep, { game with Players = newPlayers })
+//        | _ -> (nextStep, { game with Players = newPlayers })
