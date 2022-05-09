3ac2e98d98ca3cb3ea38b2d843326e9ef340dab5
d.zschornak <d.zschornak@gmail.com>
2022-05-09 15:54:13 +0200

    move Game get current player form game logic to types
    Signed-off-by: d.zschornak <d.zschornak@gmail.com>

--- src/Client/Player.fs
+++ src/Client/Player.fs
@@ -6,38 +6,67 @@ open Shared
 
 module Player =
     let renderPlayers (g: Game) =
-            let p = g |> Game.getPlayers
-            let currentPlayerIndex = g |> Game.getCurrentPlayerIndex
-            let filledList =
-                p |>
-                List.map (fun p ->
-                    (p |> Player.getCurrentLeg).Records |> List.rev |> List.chunkBySize 3
-                )
+        let p = g |> Game.getPlayers
+
+        let currentPlayerIndex =
+            g |> Game.getCurrentPlayerIndex
+
+        let filledList =
             p
-            |> List.mapi (fun i p ->
-                Bulma.container [
-                    match i = currentPlayerIndex with
-                    | true -> prop.className "player-active"
-                    | _ -> prop.className "player-inactive"
-                    prop.children [
-                        Bulma.columns [
-                            Bulma.column [
-                                column.is3
-                                prop.children [
-                                    Bulma.text.span (sprintf "\u2300")
-                                ]
+            |> List.map (fun p ->
+                (p |> Player.getCurrentLeg).Records
+                |> List.rev
+                |> List.chunkBySize 3)
+            |> List.head
+            |> List.concat
+
+
+        p
+        |> List.mapi (fun i p ->
+            Bulma.container [
+                match i = currentPlayerIndex with
+                | true -> prop.className "player-active"
+                | _ -> prop.className "player-inactive"
+                prop.children [
+                    Bulma.columns [
+                        Bulma.column [
+                            column.is3
+                            prop.children [
+                                Bulma.text.span (sprintf "\u2300")
                             ]
-                            Bulma.column [
-                                column.is6
-                                text.hasTextCentered
-                                prop.children [ Bulma.text.span p.Name ]
-                            ]
-                            Bulma.column [
-                                column.is3
-                                prop.children [
-                                    Bulma.text.span p.Legs.Head.CurrentScore
-                                ]
+                        ]
+                        Bulma.column [
+                            column.is6
+                            text.hasTextCentered
+                            prop.children [ Bulma.text.span p.Name ]
+                        ]
+                        Bulma.column [
+                            column.is3
+                            prop.children [
+                                Bulma.text.span p.Legs.Head.CurrentScore
                             ]
                         ]
                     ]
-                ]) |> Fable.React.Helpers.ofList
+                    Bulma.columns [
+                        filledList
+                        |> List.map (fun s ->
+                            Bulma.column [
+                                column.is4
+                                prop.children [
+                                    Bulma.tag [
+                                        prop.style [
+                                            style.width (length.percent 100)
+                                            style.marginBottom 5
+                                        ]
+                                        tag.isLarge
+                                        tag.isRounded
+                                        color.hasBackgroundGreyLight
+                                        prop.text $"%s{s |> Shot.ToString}"
+                                    ]
+                                ]
+                            ])
+                        |> Fable.React.Helpers.ofList
+                    ]
+                ]
+            ])
+        |> Fable.React.Helpers.ofList
