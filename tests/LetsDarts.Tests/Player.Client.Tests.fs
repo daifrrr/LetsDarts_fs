82081425543dc44d863da4a1f87facb20b272670
d.zschornak <d.zschornak@gmail.com>
2022-05-10 10:10:39 +0200

    rename LetsDartsCore to LetsDarts
    Signed-off-by: d.zschornak <d.zschornak@gmail.com>

---
+++ tests/LetsDarts.Tests/Player.Client.Tests.fs
@@ -0,0 +1,13 @@
+﻿namespace LetsDartsCore.Client.Tests
+
+open FsUnit.Xunit
+open Xunit
+
+
+module Player =
+    [<Fact>]
+    let ``Empty list returns [" "; " "; " "]`` () =
+        let expected = true
+        let actual = true
+        actual |> should equal expected
+
