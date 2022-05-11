3f1f8e60b1fb2d31b85b4eb4a31b89b29012e59f
d.zschornak <d.zschornak@gmail.com>
2022-05-11 13:43:49 +0200

    remove Logger from Helpers
    Signed-off-by: d.zschornak <d.zschornak@gmail.com>

---
+++ src/Client/Players.fs
@@ -0,0 +1,69 @@
+﻿namespace Client.Components
+
+open Feliz
+open Feliz.Bulma
+open Shared
+
+module Players =
+
+    let previousScore (p:Player) =
+        let previousRecord = (p |> Player.getCurrentLeg).Records
+                             |> List.chunkBySize 3
+                             |> List.head
+        let previousScore = previousRecord |> Leg.calcCurrentScore
+        Bulma.container [
+            prop.className "dev"
+            prop.children [
+                Bulma.content [
+                    Html.h2 $"%d{previousScore}"
+                ]
+                Bulma.columns [
+                    previousRecord |> List.map (fun s ->
+                        Bulma.column [
+                            column.isOneThird
+                            prop.children [
+                                Bulma.content [
+                                    Html.h6 $"%s{s |> ToString}"
+                                ]
+                            ]
+                        ]
+                    ) |> Fable.React.Helpers.ofList
+                ]
+            ]
+        ]
+    let renderPlayers (g: Game) =
+        let p = g |> Game.getPlayers
+
+        let currentPlayerIndex =
+            g |> Game.getCurrentPlayerIndex
+
+        Bulma.container [
+            prop.children [
+                Bulma.columns [
+                    prop.className "players"
+                    prop.children [
+                        p
+                        |> List.mapi (fun i p ->
+                            Bulma.column [
+                                prop.className "player"
+                                prop.children [
+                                    Bulma.content [
+                                        match i = currentPlayerIndex with
+                                        | true -> prop.className "active"
+                                        | _ -> prop.className "not-active"
+                                        text.hasTextCentered
+                                        prop.children [
+                                            Html.h3 [ prop.text $"{p.Name}" ]
+                                            previousScore p
+                                            Html.h1 [ prop.text $"{(p |> Player.getCurrentLeg).CurrentScore}" ]
+                                            Html.h2 [ prop.text $"26,1" ]
+
+                                            ]
+                                    ]
+                                ]
+                            ])
+                        |> Fable.React.Helpers.ofList
+                    ]
+                ]
+            ]
+        ]
--- src/Shared/Helpers.fs
+++ src/Shared/Helpers.fs
@@ -1,44 +1,4 @@
-﻿[<AutoOpen>]
-module Shared.Helpers
-
-
-module Logger =
-    /// Log levels.
-    let Error = 0
-    let Warning = 1
-    let Information = 2
-    let Debug = 3
-
-    let LevelToString level =
-      match level with
-        | 0 -> "Error"
-        | 1 -> "Warning"
-        | 2 -> "Information"
-        | 3 -> "Debug"
-        | _ -> "Unknown"
-
-    /// The current log level.
-    let mutable current_log_level = Debug
-
-    /// The inteface loggers need to implement.
-    type ILogger = abstract Log : int -> Printf.StringFormat<'a,unit> -> 'a
-
-    /// Writes to console.
-    let ConsoleLogger = {
-      new ILogger with
-        member _.Log level format =
-          Printf.kprintf (printfn "[%s][%A] %s" (LevelToString level) System.DateTime.Now) format
-     }
-
-    /// Defines which logger to use.
-    let mutable DefaultLogger = ConsoleLogger
-
-    /// Logs a message with the specified logger.
-    let logUsing (logger: ILogger) = logger.Log
-
-    /// Logs a message using the default logger.
-    let log level message = logUsing DefaultLogger level message
-
+﻿namespace Shared.Helpers
 module UsefulMaths =
     // https://math.stackexchange.com/questions/1387033/formula-for-alternating-sequences
     let PingPong a b n =
