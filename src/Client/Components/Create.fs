f135a8d3f949cdcc12c6a0a07148af322c261f4b
philippballin <philippe@corleone-media.de>
2022-05-23 16:57:28 +0200

    format

--- src/Client/Components/Create.fs
+++ src/Client/Components/Create.fs
@@ -10,116 +10,136 @@ open Shared
 module Create =
     let Form (model: Model) (dispatch: Msg -> unit) =
         Html.div [
-            Html.div [
-                prop.className "frm-row1"
-                prop.children [
-                    Html.div [
-                        column.is6
-                        prop.className "frm-mode"
-                        prop.children [
-                            Html.label [
-                                prop.text "Mode"
-                            ]
-                            Html.select [
-                                prop.value (model.Game.Mode |> string)
-                                prop.onChange (ChangeMode >> dispatch)
-                                prop.children [
-                                    Html.option 301
-                                    Html.option 501
-                                ]
-                            ]
-                        ]
-                    ]
-                    Html.div [
-                        prop.className "frm-mode"
-                        prop.children [
-                            Html.label [
-                                prop.text "First To Legs"
-                            ]
-                            Html.select [
-                                prop.value model.Game.Legs
-                                prop.onChange (ChangeCountOfLegs >> dispatch)
-                                prop.children [
-                                    Html.option 1
-                                    Html.option 3
-                                    Html.option 5
-                                    Html.option 7
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-            Html.div [
-                prop.className "frm-row2"
-                prop.children [
-                    Html.div [
-                        prop.className "frm-dbl-in display-none"
-                        prop.children [
-                            Html.label [
-                                Html.input [
-                                    prop.type' "checkbox"
-                                    prop.onCheckedChange (SwitchDoubleIn >> dispatch)
-                                    prop.isChecked model.Game.DoubleIn
-                                ]
-                                Html.span [
-                                    prop.text "Double In"
-                                ]
-                            ]
-                        ]
-                    ]
-                    Html.div [
-                        prop.className "frm-dbl-out display-none"
-                        prop.children [
-                            Html.label [
-                                Html.input [
-                                    prop.type' "checkbox"
-                                    prop.onCheckedChange (SwitchDoubleOut >> dispatch)
-                                    prop.isChecked model.Game.DoubleOut
-                                ]
-                                Html.span [
-                                    prop.text "Double Out"
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-            Html.div [
-                prop.className "frm-players"
-                prop.children [
-                    model.Game.Players
-                    |> List.mapi (fun i p ->
+            prop.className "container-fluid create-layer"
+            prop.children [
+                Html.div [
+                    prop.className "row"
+                    prop.children [
                         Html.div [
+                            prop.className "col-4"
+                            prop.children [
+                                Html.label [
+                                    prop.text "Score"
+                                ]
+                            ]
+                        ]
+                        Html.div [
+                            prop.className "col-4"
+                            prop.children [
+                                Html.label [
+                                    prop.text "Sets"
+                                ]
+                            ]
+                        ]
+                        Html.div [
+                            prop.className "col-4"
+                            prop.children [
+                                Html.label [
+                                    prop.text "Legs"
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+                Html.div [
+                    prop.className "row"
+                    prop.children [
+                        Html.div [
+                            prop.className "col-4"
                             prop.children [
                                 Html.div [
-                                    prop.className "is-full"
+                                    column.is6
+                                    prop.className "ld-input-score"
                                     prop.children [
-                                        Html.input [
-                                            prop.type' "text"
-                                            prop.className "ld-input ld-player-name-input"
-                                            prop.placeholder p.Name
-                                            prop.custom ("index", i)
-                                            prop.onChange (handleInput i)
+                                        Html.select [
+                                            prop.value (model.Game.Mode |> string)
+                                            prop.onChange (ChangeMode >> dispatch)
+                                            prop.children [
+                                                Html.option 301
+                                                Html.option 501
+                                            ]
                                         ]
                                     ]
                                 ]
                             ]
-                        ])
-                    |> Fable.React.Helpers.ofList
-                    Html.button [
-                        prop.className "btn-player-plus"
-                        prop.onClick addPlayer
-                        prop.text "+"
+                        ]
                     ]
                 ]
-            ]
-            Html.div [
-                prop.children [
-                    Html.button [
-                        prop.className "btn-game-start"
-                        prop.text "Start"
-                        prop.onClick (fun _ -> dispatch OrderPlayers)
+                Html.div [
+                    prop.className "col-4"
+                    prop.children [
+                        Html.div [
+                            prop.className "ld-input-sets"
+                            prop.children [
+                                Html.select [
+                                    prop.onChange (ChangeCountOfLegs >> dispatch)
+                                    prop.children [
+                                        Html.option 1
+                                        Html.option 3
+                                        Html.option 5
+                                        Html.option 7
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+                Html.div [
+                    prop.className "col-4"
+                    prop.children [
+                        Html.div [
+                            prop.className "ld-input-legs"
+                            prop.children [
+                                Html.select [
+                                    prop.value model.Game.Legs
+                                    prop.onChange (ChangeCountOfLegs >> dispatch)
+                                    prop.children [
+                                        Html.option 1
+                                        Html.option 3
+                                        Html.option 5
+                                        Html.option 7
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+                Html.div [
+                    prop.className "frm-players"
+                    prop.children [
+                        model.Game.Players
+                        |> List.mapi (fun i p ->
+                            Html.div [
+                                prop.children [
+                                    Html.div [
+                                        prop.className "is-full"
+                                        prop.children [
+                                            Html.input [
+                                                prop.type' "text"
+                                                prop.className "ld-input ld-player-name-input"
+                                                prop.placeholder p.Name
+                                                prop.custom ("index", i)
+                                                prop.onChange (handleInput i)
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ])
+                        |> Fable.React.Helpers.ofList
+                        Html.button [
+                            prop.className "btn-player-plus"
+                            prop.onClick addPlayer
+                            prop.text "+"
+                        ]
+                        Html.div [
+                            prop.children [
+                                Html.button [
+                                    prop.className "btn-game-start"
+                                    prop.text "Start"
+                                    prop.onClick (fun _ -> dispatch OrderPlayers)
+                                ]
+                            ]
+                        ]
                     ]
                 ]
             ]
