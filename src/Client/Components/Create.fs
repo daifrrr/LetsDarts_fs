2c970e1bbcaebe1e731f025854099b4119480141
philippballin <philippe@corleone-media.de>
2022-05-23 14:27:44 +0200

    adjustment for style

--- src/Client/Components/Create.fs
+++ src/Client/Components/Create.fs
@@ -9,71 +9,118 @@ open Shared
 [<RequireQualifiedAccess>]
 module Create =
     let Form (model: Model) (dispatch: Msg -> unit) =
-        Bulma.container [ Bulma.columns [ prop.className "frm-row1"
-                                          prop.children [ Bulma.column [ prop.className "frm-mode"
-                                                                         prop.children [ Bulma.label "Mode"
-                                                                                         Bulma.select [ prop.value (
-                                                                                                            model.Game.Mode
-                                                                                                            |> string
-                                                                                                        )
-                                                                                                        prop.onChange (
-                                                                                                            ChangeMode
-                                                                                                            >> dispatch
-                                                                                                        )
-                                                                                                        prop.children [ Html.option
-                                                                                                                            301
-                                                                                                                        Html.option
-                                                                                                                            501 ] ] ] ]
-                                                          Bulma.column [ prop.className "frm-mode"
-                                                                         prop.children [ Bulma.label "First To Legs"
-                                                                                         Bulma.select [ prop.value
-                                                                                                            model.Game.Legs
-                                                                                                        prop.onChange (
-                                                                                                            ChangeCountOfLegs
-                                                                                                            >> dispatch
-                                                                                                        )
-                                                                                                        prop.children [ Html.option
-                                                                                                                            1
-                                                                                                                        Html.option
-                                                                                                                            3
-                                                                                                                        Html.option
-                                                                                                                            5
-                                                                                                                        Html.option
-                                                                                                                            7 ] ] ] ] ] ]
-                          Bulma.columns [ prop.className "frm-row2"
-                                          prop.children [ Bulma.column [ prop.className "frm-dbl-in"
-                                                                         prop.children [ Bulma.label [ Bulma.input.checkbox [ prop
-                                                                                                                                  .onCheckedChange (
-                                                                                                                                      SwitchDoubleIn
-                                                                                                                                      >> dispatch
-                                                                                                                                  )
-                                                                                                                              prop.isChecked
-                                                                                                                                  model.Game.DoubleIn ]
-                                                                                                       Bulma.text.span
-                                                                                                           "Double In" ] ] ]
-                                                          Bulma.column [ prop.className "frm-dbl-out"
-                                                                         prop.children [ Bulma.label [ Bulma.input.checkbox [ prop
-                                                                                                                                  .onCheckedChange (
-                                                                                                                                      SwitchDoubleOut
-                                                                                                                                      >> dispatch
-                                                                                                                                  )
-                                                                                                                              prop.isChecked
-                                                                                                                                  model.Game.DoubleOut ]
-                                                                                                       Bulma.text.span
-                                                                                                           "Double Out" ] ] ] ] ]
-                          Bulma.columns [ prop.className "frm-players"
-                                          prop.children [ Bulma.label "Players"
-                                                          model.Game.Players
-                                                          |> List.mapi (fun i p ->
-                                                              Bulma.input.text [ prop.className "frm-player"
-                                                                                 prop.placeholder p.Name
-                                                                                 prop.custom ("index", i)
-                                                                                 prop.onChange (handleInput i) ])
-                                                          |> Fable.React.Helpers.ofList
-                                                          Bulma.button.span [ prop.className "btn-player-plus"
-                                                                              prop.onClick addPlayer
-                                                                              prop.text "+" ] ] ]
-                          Bulma.columns [ prop.children [ Bulma.button.span [ prop.className "btn-game-start"
-                                                                              prop.text "Start"
-                                                                              prop.onClick (fun _ ->
-                                                                                  dispatch OrderPlayers) ] ] ] ]
+        Html.div [
+            Html.div [
+                prop.className "frm-row1"
+                prop.children [
+                    Html.div [
+                        column.is6
+                        prop.className "frm-mode"
+                        prop.children [
+                            Html.label [
+                                prop.text "Mode"
+                            ]
+                            Html.select [
+                                prop.value (model.Game.Mode |> string)
+                                prop.onChange (ChangeMode >> dispatch)
+                                prop.children [
+                                    Html.option 301
+                                    Html.option 501
+                                ]
+                            ]
+                        ]
+                    ]
+                    Html.div [
+                        prop.className "frm-mode"
+                        prop.children [
+                            Html.label [
+                                prop.text "First To Legs"
+                            ]
+                            Html.select [
+                                prop.value model.Game.Legs
+                                prop.onChange (ChangeCountOfLegs >> dispatch)
+                                prop.children [
+                                    Html.option 1
+                                    Html.option 3
+                                    Html.option 5
+                                    Html.option 7
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+            Html.div [
+                prop.className "frm-row2"
+                prop.children [
+                    Html.div [
+                        prop.className "frm-dbl-in display-none"
+                        prop.children [
+                            Html.label [
+                                Html.input [
+                                    prop.type' "checkbox"
+                                    prop.onCheckedChange (SwitchDoubleIn >> dispatch)
+                                    prop.isChecked model.Game.DoubleIn
+                                ]
+                                Html.span [
+                                    prop.text "Double In"
+                                ]
+                            ]
+                        ]
+                    ]
+                    Html.div [
+                        prop.className "frm-dbl-out display-none"
+                        prop.children [
+                            Html.label [
+                                Html.input [
+                                    prop.type' "checkbox"
+                                    prop.onCheckedChange (SwitchDoubleOut >> dispatch)
+                                    prop.isChecked model.Game.DoubleOut
+                                ]
+                                Html.span [
+                                    prop.text "Double Out"
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+            Html.div [
+                prop.className "frm-players"
+                prop.children [
+                    model.Game.Players
+                    |> List.mapi (fun i p ->
+                        Html.div [
+                            prop.children [
+                                Html.div [
+                                    prop.className "is-full"
+                                    prop.children [
+                                        Html.input [
+                                            prop.type' "text"
+                                            prop.className "ld-input ld-player-name-input"
+                                            prop.placeholder p.Name
+                                            prop.custom ("index", i)
+                                            prop.onChange (handleInput i)
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ])
+                    |> Fable.React.Helpers.ofList
+                    Html.button [
+                        prop.className "btn-player-plus"
+                        prop.onClick addPlayer
+                        prop.text "+"
+                    ]
+                ]
+            ]
+            Html.div [
+                prop.children [
+                    Html.button [
+                        prop.className "btn-game-start"
+                        prop.text "Start"
+                        prop.onClick (fun _ -> dispatch OrderPlayers)
+                    ]
+                ]
+            ]
+        ]
