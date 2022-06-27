52393b999d2d51aef2d495178c0f44348090f564
d.zschornak <d.zschornak@gmail.com>
2022-06-27 11:10:06 +0200

    feat: drag and drop ugly example
    Signed-off-by: d.zschornak <d.zschornak@gmail.com>

--- src/Client/Components/Sort.fs
+++ src/Client/Components/Sort.fs
@@ -12,42 +12,55 @@ module Sort =
     let dragAndDropConfig =
         { DragAndDropConfig.Empty() with
             DraggedElementStyles =
-                Some []
+                Some [
+                    Opacity 0.8
+                    Position PositionOptions.Fixed
+                    Cursor "grabbing"
+                ]
             HoverPreviewElementStyles = Some [ Opacity 0.2 ] }
 
     let mappedMsg msg = DndMsg msg
+
     let Form (model: Model) (dispatch: Msg -> unit) =
-        let dispatch = (mappedMsg >> dispatch)
+        let dispatchDnDMsg = (mappedMsg >> dispatch)
 
         let dropAreaContent =
             model.DragAndDrop.ElementIdsForCategorySingleList dragAndDropCategoryKey
             |> List.collect (fun id ->
                 let content =
                     Map.tryFind id model.ContentMap
-                    |> Option.defaultValue (Fable.React.Standard.div [] [Fable.React.Helpers.str "Unable to find content"])
+                    |> Option.defaultValue (
+                        Fable.React.Standard.div [] [
+                            Fable.React.Helpers.str "Unable to find content"
+                        ]
+                    )
+
                 Draggable.SelfHandle
                     model.DragAndDrop
                     dragAndDropCategoryKey
                     dragAndDropConfig
-                    dispatch
+                    dispatchDnDMsg
                     id
                     Fable.React.Standard.div
                     [ Cursor "grap" ]
-                    [ ClassName "WTFFFFFF"; Id id]
+                    [ ClassName "WTFFFFFF"; Id id ]
                     [ content ])
 
-        let dropArea = DropArea.DropArea
-                            model.DragAndDrop
-                            dragAndDropCategoryKey
-                            dragAndDropConfig
-                            (MouseEventHandlers.Empty())
-                            dispatch
-                            "player-list"
-                            Fable.React.Standard.div
-                            [ Class "player-list" ]
-                            dropAreaContent
+        let dropArea =
+            DropArea.DropArea
+                model.DragAndDrop
+                dragAndDropCategoryKey
+                dragAndDropConfig
+                (MouseEventHandlers.Empty())
+                dispatchDnDMsg
+                "player-list"
+                Fable.React.Standard.div
+                [ Class "player-list" ]
+                dropAreaContent
 
-
-        DragDropContext.Context model.DragAndDrop dispatch Fable.React.Standard.div [
-            Class "container-fluid row g-0 sort-layer"
-        ] [ dropArea ]
+        DragDropContext.Context
+            model.DragAndDrop
+            dispatchDnDMsg
+            Fable.React.Standard.div
+            [ Class "container-fluid row g-0 sort-layer" ]
+            [ dropArea ]
